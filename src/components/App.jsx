3de0cf9666ffd64f0b873daf11cb8e414314e90c
Igor-Konyaga <konyaga.igor10@gmail.com>
2023-09-28 01:08:36 +0300

    change task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactBook } from './Contact-book/ContactBook';
+import css from './App.module.css';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    name: '',
+    number: '',
+    filter: '',
+  };
+
+  formSubmit = data => {
+    this.state.contacts.push(data);
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div className={css.section}>
+        <ContactBook
+          contacts={this.state.contacts}
+          onSubmit={this.formSubmit}
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/Contact-book/ContactBook.jsx
@@ -0,0 +1,97 @@
+import { Children, Component } from 'react';
+import css from './ContactBook.module.css';
+import { render } from '@testing-library/react';
+import { nanoid } from 'nanoid';
+
+export class ContactBook extends Component {
+  state = {
+    name: '',
+    number: '',
+    id: '',
+    filter: '',
+  };
+
+  handleInput = e => {
+    const currentValue = e.target.value;
+    const currentName = e.target.name;
+    this.setState({ [currentName]: currentValue, id: nanoid() });
+  };
+
+  onSearch = e => {
+    const currentValue = e.target.value;
+    this.setState({ filter: currentValue });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state);
+
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h1 className={css.title}>Phonebooks</h1>
+        <form action="#" onSubmit={this.handleSubmit} className={css.form}>
+          <label className={css.label}>
+            Name
+            <input
+              className={css.input}
+              type="text"
+              onChange={this.handleInput}
+              value={this.state.name}
+              name="name"
+              required
+            />
+          </label>
+          <label className={css.label}>
+            Number
+            <input
+              className={css.input}
+              type="tel"
+              onChange={this.handleInput}
+              value={this.state.number}
+              name="number"
+              required
+            />
+          </label>
+
+          <button className={css.btn} type="submit">
+            add contact
+          </button>
+        </form>
+
+        <label className={css.label}>
+          Find contacts by name
+          <input
+            className={css.input}
+            onChange={this.onSearch}
+            value={this.state.filter}
+            type="text"
+          />
+        </label>
+
+        <h2 className={css.title}>Contacts</h2>
+        <ul className={css.list}>
+          {this.props.contacts.map(contact => {
+            return (
+              <li className={css.listItem} key={contact.id}>
+                <p className={css.itemName}>{contact.name}:</p>
+                <p className={css.itemTel}>{contact.number}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </>
+    );
+  }
+}
